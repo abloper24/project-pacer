e44f0adc9ebcc27bb8dbcaf088b08e24d4665567
abloper24 <ablobelperez@gmail.com>
2024-03-19 17:57:26 -0700

    basic header structure

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,32 +7,31 @@ import { NavLink } from 'react-router-dom';
 function Header() {
 
     return (
-        <>
-            <header>
-                <img src={logo} />
+        <header className="header">
+            <img src={logo} />
 
-                <nav>
-                    <NavLink to={'/'} alt=''>
-                        <button>
-                            Timer <img class src={timer} alt="timer icon" />
-                        </button>
-                    </NavLink>
+            <nav className="nav">
+                <NavLink to={'/'} className='nav__link'>
+                    <button className="nav__btn">
+                        Timer <img class src={timer} alt="timer icon" />
+                    </button>
+                </NavLink>
 
-                    <NavLink to={'/entries'} alt=''>
-                        <button>
-                            Time Entries <img src={entries} alt="entries icon" />
-                        </button>
-                    </NavLink>
+                <NavLink to={'/entries'} className='nav__link'>
+                    <button className="nav__btn">
+                        Time Entries <img src={entries} alt="entries icon" />
+                    </button>
+                </NavLink>
 
-                    <NavLink to={'/invoices'} alt=''>
-                        <button>
-                            Invoices <img src={invoices} alt="invoices icon" />
-                        </button>
-                    </NavLink>
+                <NavLink to={'/invoices'} className='nav__link'>
+                    <button className="nav__btn">
+                        Invoices <img src={invoices} alt="invoices icon" />
+                    </button>
+                </NavLink>
 
-                </nav>
-            </header>
-        </>
+            </nav>
+        </header>
+
     )
 }
 

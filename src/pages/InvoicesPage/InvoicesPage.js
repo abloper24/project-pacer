e36c075a911f5e2fec66f136c394163f7b1cc5ac
abloper24 <ablobelperez@gmail.com>
2024-03-22 15:39:00 -0700

    added pdf design and export functionality

--- src/pages/InvoicesPage/InvoicesPage.js
+++ src/pages/InvoicesPage/InvoicesPage.js
@@ -1,21 +1,26 @@
 
 import React, { useState, useEffect } from "react";
-import jsPDF from 'jspdf';
-// import 'jspdf-autotable';
+import andreaLogo from '../../assets/images/logos/andrea-logo.png'
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
+
 
 
 function InvoicesPage({ selectedEntries, clients }) {
+      // console.log(selectedEntries)
     const [chosenClientId, setChosenClientId] = useState("");
     const [invoiceDate, setInvoiceDate] = useState("");
     const [invoiceDueDate, setInvoiceDueDate] = useState("");
     const [rates, setRates] = useState({});
     const [taxRates, setTaxRates] = useState({});
-    const [terms, setTerms] = useState("If payment is not received within 10 days of the due date, the Client will also be subject to a late fee of 5% of the amount overdue.\n\nGST#: 702288010RT0001");
+    const [terms, setTerms] = useState("If payment is not received within 10 days of the due date, the Client will also be subject to a late fee of 5% of the amount overdue.\n\nGST/HST#: 702288010RT0001");
     const [paymentInstructions, setPaymentInstructions] = useState("Please send payment via e-transfer to ablobelperez@gmail.com");
     const [selectedTaxRate, setSelectedTaxRate] = useState(0);
+    const [invoiceNumber, setInvoiceNumber] = useState("");
 
-    // console.log(selectedEntries)
+    const clientForInvoice = clients.find(client => client.clientid === selectedEntries[0]?.clientid) || {};
 
+  
     const handleDateChange = (event) => {
         setInvoiceDate(event.target.value);
     };
@@ -34,6 +39,10 @@ function InvoicesPage({ selectedEntries, clients }) {
 
     const handleTaxRateChange = (e) => {
         setSelectedTaxRate(Number(e.target.value));
+    };
+
+    const handleInvoiceNumberChange = (event) => {
+        setInvoiceNumber(event.target.value);
     };
 
     // round seconds up to min and then to h
@@ -89,21 +98,139 @@ function InvoicesPage({ selectedEntries, clients }) {
         }
     };
 
+    // build  PDF
+    const generatePDF = () => {
+        const doc = new jsPDF();
+        doc.setFont("Helvetica");
+    
+        // logo image
+        const logoWidth = 50;
+        const logoHeight = 50;
+        const yPosition = 10;
+        doc.addImage(andreaLogo, 'PNG', 15, yPosition, logoWidth, logoHeight);
+    
+        // address info
+        let pageWidth = doc.internal.pageSize.getWidth();
+        doc.setFontSize(11);
+        const rightMargin = 10;
+        const addressYStart = 30;
+        const lineHeight = 5;
+        let addressXPosition = pageWidth - rightMargin;
+        let addressLines = [
+            'Andrea Blobel Pérez',
+            '1003-1188 Bidwell',
+            'Vancouver British Columbia V6G 0C6',
+            'Canada',
+            '672-515-3544',
+            'billing@andreablobel.com'
+        ];
+    
+        // printing Address Lines
+        addressLines.forEach((line, i) => {
+            doc.text(line, addressXPosition, addressYStart + (i * lineHeight), null, null, 'right');
+        });
+    
+        // billing info
+        let billingInfoLine = [
+            `Bill to: ${clientForInvoice.name || 'Client not selected'}`,
+            `Email: ${clientForInvoice.email || 'Email not provided'}`,
+            `Phone: ${clientForInvoice.phone || 'Phone not provided'}`,
+        ];
+        let billingStartY = addressYStart + (addressLines.length * lineHeight) + 10;
+
+        // priting billing info
+        billingInfoLine.forEach((line, i) => {
+            doc.text(line, 15, billingStartY + (i * lineHeight)); 
+        });
+
+        //invide details info
+        let invoiceInfoLine = [
+            `Invoice #: ${invoiceNumber}`,
+            `Invoice Date: ${invoiceDate}`,
+            `Due Date: ${invoiceDueDate}`,
+        ];
+        //determine invoice info position in pdf - aligned with billingstartY axis but right
+        //taking x position of address
+        let invoiceInfoStartY = billingStartY;
+        let invoiceInfoX = addressXPosition;
+        //printing invoice info
+        invoiceInfoLine.forEach((line, i) => {
+            doc.text(line, invoiceInfoX, invoiceInfoStartY + (i * lineHeight), null, null, 'right');
+        });
+        
+        //position of table
+        let tableStartY = invoiceInfoStartY + (billingInfoLine.length * lineHeight) + 10;
+    
+        // table headers and data
+        let headers = ["#", "Task", "Project Hours", "Rate", "Amount"];
+        let data = selectedEntries.map((selectedEntry, i) => [
+            i + 1,
+            selectedEntry.description,
+            roundedMinutesIntoHours(selectedEntry.duration),
+            `$${rates[selectedEntry.timerid] || '0.00'}`,
+            `$${calculateAmountWithTax(selectedEntry.duration, rates[selectedEntry.timerid] || 0, taxRates[selectedEntry.timerid] || 0)}`
+        ]);
+
+        // printing table
+        doc.autoTable({
+            head: [headers],
+            body: data,
+            startY: tableStartY,
+            theme: 'grid',
+            styles: {
+                fontSize: 11,
+                cellPadding: 2,
+                font: 'Helvetica',
+                textColor: [0, 0, 0]
+            },
+            headStyles: {
+                fillColor: [245, 246, 247],
+                textColor: [0, 0, 0],
+                fontSize: 11,
+                fontStyle: 'bold',
+                font: 'Helvetica'
+            },
+            margin: { right: rightMargin }
+        });
+
+        // Subtotal, Tax, and Total 
+        let financialDetailsStartY = doc.previousAutoTable.finalY + 10;
+        const financialLineHeight = 6;
+        doc.text(`Subtotal: $${subtotal}`, addressXPosition, financialDetailsStartY, null, null, 'right');
+        doc.text(`${getTaxName(selectedTaxRate)}: $${taxAmount}`, addressXPosition, financialDetailsStartY + financialLineHeight, null, null, 'right');
+        doc.text(`Balance due: $${totalAmount}`, addressXPosition, financialDetailsStartY + 2 * financialLineHeight, null, null, 'right');
+
+        // Terms & Conditions 
+        let termsStartY = financialDetailsStartY + 3 * financialLineHeight + 10; 
+        doc.setFont("Helvetica", "bold");
+        doc.text("Terms & Conditions", 14, termsStartY);
+        doc.setFont("Helvetica", "normal");
+        let termsLines = doc.splitTextToSize(terms, 180);
+        termsLines.forEach((line, index) => {
+            doc.text(line, 14, termsStartY + 7 + (index * 6.5)); 
+        });
+
+        // Payment
+        let paymentInstructionsStartY = termsStartY + 7 + (termsLines.length * 6.5) + 10; 
+        doc.setFont("Helvetica", "bold");
+        doc.text("Payment Instructions", 14, paymentInstructionsStartY);
+        doc.setFont("Helvetica", "normal");
+        let paymentLines = doc.splitTextToSize(paymentInstructions, 180); 
+        paymentLines.forEach((line, index) => {
+            doc.text(line, 14, paymentInstructionsStartY + 7 + (index * 6.5)); 
+        });
+
+        // save  PDF
+        doc.save(`${invoiceNumber}.pdf`);
+    };
+
+
     return (
         <div>
             <h1>Invoices</h1>
             <form>
                 <div>
-                    <select
-                        value={chosenClientId}
-                        onChange={(e) => setChosenClientId(e.target.value)}
-                    >
-                        {clients.map((client) => (
-                            <option key={client.clientid} value={client.clientid}>
-                                {client.name}
-                            </option>
-                        ))}
-                    </select>
+                    <p>Client: {clientForInvoice.name || 'Client not selected'}</p>
                 </div>
 
                 <div>
@@ -112,6 +239,8 @@ function InvoicesPage({ selectedEntries, clients }) {
                         type="text"
                         placeholder="INV-00099"
                         name="invoice-number"
+                        value={invoiceNumber}
+                        onChange={handleInvoiceNumberChange}
                     >
                     </input>
                 </div>
@@ -199,7 +328,15 @@ function InvoicesPage({ selectedEntries, clients }) {
                     />
                 </div>
 
+                <button type="button" onClick={generatePDF}>
+                    Export to PDF
+                </button>
+
+
+
             </form>
+
+
 
         </div>
     );

dba3a4033bb4048f41acbf0cea8227a8e5c9d6cf
abloper24 <ablobelperez@gmail.com>
2024-03-22 09:28:36 -0700

    added minimal calculation of rate

--- src/pages/InvoicesPage/InvoicesPage.js
+++ src/pages/InvoicesPage/InvoicesPage.js
@@ -7,12 +7,34 @@ import jsPDF from 'jspdf';
 function InvoicesPage({ selectedEntries, clients }) {
     const [chosenClientId, setChosenClientId] = useState("");
     const [invoiceDate, setInvoiceDate] = useState("");
+    const [invoiceDueDate, setInvoiceDueDate] = useState("");
+    const [rates, setRates] = useState({});
 
     // console.log(selectedEntries)
 
     const handleDateChange = (event) => {
         setInvoiceDate(event.target.value);
-      };
+    };
+
+    const handleDueDateChange = (event) => {
+        setInvoiceDueDate(event.target.value);
+    };
+
+    const handleRateChange = (timerId, rate) => {
+        setRates(prevRates => ({ ...prevRates, [timerId]: rate }));
+    };
+
+    // round seconds up to min and then to h
+    const roundedMinutesIntoHours = (seconds) => {
+        const minutes = Math.ceil(seconds / 60); 
+        const hours = minutes / 60; 
+        return hours.toFixed(2); //  2 decimal places
+    };
+
+    const calculateAmount = (durationInSeconds, rate) => {
+        const hours = roundedMinutesIntoHours(durationInSeconds);
+        return (hours * rate).toFixed(2);
+    };
 
     return (
         <div>
@@ -35,7 +57,7 @@ function InvoicesPage({ selectedEntries, clients }) {
                     <label>Invoice #</label>
                     <input
                         type="text"
-                        placeholder="INV-000071"
+                        placeholder="INV-00099"
                         name="invoice-number"
                     >
                     </input>
@@ -43,33 +65,63 @@ function InvoicesPage({ selectedEntries, clients }) {
                 <div>
                     <label>Invoice Date</label>
                     <input
-                        type="text"
-                        placeholder="2024/02/25"
-                        name="invoice-date"
-                    >
-                    </input>
-
-                    <label>Due Date</label>
-                    <input
                         type="date"
                         id="invoice-date"
                         name="invoice-date"
                         value={invoiceDate}
                         onChange={handleDateChange}
                     />
+
+                    <label>Due Date</label>
+                    <input
+                        type="date"
+                        id="invoice-due-date"
+                        name="invoice-due-date"
+                        value={invoiceDueDate}
+                        onChange={handleDueDateChange}
+                    />
                 </div>
 
                 <div>
                     {selectedEntries.map((selectedEntries) => (
                         <div key={selectedEntries.timerid}>
-                            <div>Date: {selectedEntries.starttime.slice(0, 10)}</div>
-                            <div>StartTime: {selectedEntries.starttime.slice(11, 19)}</div>
-                            <div>EndTime: {selectedEntries.endtime.slice(11, 19)}</div>
-                            <div>Duration/Time: {selectedEntries.duration}</div>
                             <div>Task: {selectedEntries.description}</div>
                             <div>Client Name: {clients.find(client => client.clientid === selectedEntries.clientid)?.name}</div>
-                            <div>Billing Status:</div>
-                            
+                            <div>Date: {selectedEntries.starttime.slice(0, 10)}</div>
+                            <div>Duration/Time: {roundedMinutesIntoHours(selectedEntries.duration)}</div>
+
+
+
+                            <label>Rate: $</label>
+                            <input
+                                type="number"
+                                id={`rate-${selectedEntries.timerid}`}
+                                value={rates[selectedEntries.timerid] || ''}
+                                onChange={e => handleRateChange(selectedEntries.timerid, Number(e.target.value))}
+                            />
+                            <label>Tax</label>
+                            <select
+
+                            >
+
+                                <option >
+                                    GST [5%]
+                                </option>
+                                <option >
+                                    HST [15%]
+                                </option>
+
+                            </select>
+
+
+                            <label>Amount: $</label>
+                            <input
+                                type="text"
+                                id={`amount-${selectedEntries.timerid}`}
+                                value={calculateAmount(selectedEntries.duration, rates[selectedEntries.timerid] || 0)}
+                                disabled
+                            />
+
                         </div>
                     ))}
                 </div>

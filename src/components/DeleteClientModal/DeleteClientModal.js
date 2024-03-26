e8d527b1d596e8b4397b36e2bcf750070104a72e
abloper24 <ablobelperez@gmail.com>
2024-03-26 12:49:36 -0700

    delete client and delete entry modals styled

--- src/components/DeleteClientModal/DeleteClientModal.js
+++ src/components/DeleteClientModal/DeleteClientModal.js
@@ -6,16 +6,17 @@ const DeleteClientModal = ({ isOpen, onClose, onDelete, selectedClientToDelete }
   if (!isOpen) return null;
 
   return (
-    <div className="modal__container">
+    <div className="modal__container-client">
       <div className="modal__content">
         <button onClick={onClose} className="modal__close-btn">
           <img src={closeIcon} alt="close icon" />
         </button>
         <h2 className='modal__title'>Are you sure you want to delete this client?</h2>
-        <div>Name:{selectedClientToDelete.name}</div>
-        <div>Email: {selectedClientToDelete.email}</div>
-        <div>Phone: {selectedClientToDelete.phone}</div>
-        <div>Address: {selectedClientToDelete.address}</div>
+        <div>
+          <h3>Name:</h3><p>{selectedClientToDelete.name}</p></div>
+        <div><h3>Email:</h3><p>{selectedClientToDelete.email}</p> </div>
+        <div><h3>Phone: </h3><p>{selectedClientToDelete.phone}</p></div>
+        <div><h3>Address:</h3><p>{selectedClientToDelete.address}</p> </div>
         <div className="modal__btn-container">
           <button onClick={onClose} className="modal__cancel-btn">Cancel</button>
           <button onClick={onDelete} className="modal__delete-btn">Delete</button>
